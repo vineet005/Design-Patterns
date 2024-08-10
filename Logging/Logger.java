793d3317a2c7f87931b3cd9f0f48540edaa6cdf7
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-11 00:19:57 +0530

    Logger class for singleton implementation.

---
+++ Logging/Logger.java
@@ -0,0 +1,23 @@
+package Logging;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger(){
+//        System.out.println("Private Constructor of Logger Class.");
+    }
+
+    public static Logger getInstance(){
+        if (instance == null){
+            synchronized (Logger.class){
+                if (instance == null)
+                    instance = new Logger();
+            }
+        }
+        return instance;
+    }
+
+    public void log(String message){
+        System.out.println(message);
+    }
+}
