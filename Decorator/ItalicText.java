c0958c1096f88ca382ee6ec03eab3a998e45e5c3
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:43:44 +0530

    Italic Concrete Class.

--- Decorator/ItalicText.java
+++ Decorator/ItalicText.java
@@ -1,2 +1,12 @@
-package Decorator;public class ItalicText {
+package Decorator;
+
+public class ItalicText extends AbstractText{
+    public ItalicText(Text newText) {
+        super(newText);
+    }
+
+    @Override
+    public String editText() {
+        return "*" + NEW_TEXT.editText() + "*";
+    }
 }
