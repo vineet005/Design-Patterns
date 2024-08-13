bbdc2033a667975ba9c9a82253f1dec212eb98f0
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:40:22 +0530

    Meal Class.

--- Builder/MainBuilder.java
+++ Builder/MainBuilder.java
@@ -1,2 +1,17 @@
-package Builder;public class MainBuilder {
+package Builder;
+
+//Builder Pattern is used when an object needs to be created with many possible configurations and parameters,
+//some of which may be optional!
+public class MainBuilder {
+    public static void main(String[] args) {
+        MealBuilder veggie_Meal = new VeggieMeal();
+        MealDirector director = new MealDirector(veggie_Meal);
+        Meal veggie_Construct = director.construct_Meal();
+        veggie_Construct.displayMeal();
+
+        MealBuilder chicken_Meal = new ChickenMeal();
+        director = new MealDirector(chicken_Meal);
+        Meal chicken_Construct = director.construct_Meal();
+        chicken_Construct.displayMeal();
+    }
 }
--- Builder/Meal.java
+++ Builder/Meal.java
@@ -1,2 +1,23 @@
-package Builder;public class Meal {
+package Builder;
+
+public class Meal {
+    private String burger;
+    private String drink;
+    private String side;
+
+    public void setBurger(String burger) {
+        this.burger = burger;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public void displayMeal(){
+        System.out.println("Meal: " + burger + ", " + drink + ", " + side);
+    }
 }
