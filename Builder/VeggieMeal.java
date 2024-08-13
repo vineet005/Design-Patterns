00c5ac67f5ff3928ab16140dec046a4fbf4059fc
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:41:28 +0530

    Veggie Meal Class.

--- Builder/VeggieMeal.java
+++ Builder/VeggieMeal.java
@@ -1,2 +1,24 @@
-package Builder;public class VeggieMeal {
+package Builder;
+
+public class VeggieMeal implements MealBuilder{
+    private final Meal MEAL = new Meal();
+    @Override
+    public void buildBurger() {
+        MEAL.setBurger("Veggie Burger");
+    }
+
+    @Override
+    public void buildDrink() {
+        MEAL.setDrink("Ice Tea");
+    }
+
+    @Override
+    public void buildSide() {
+        MEAL.setSide("Potato Fries");
+    }
+
+    @Override
+    public Meal getMeal() {
+        return MEAL;
+    }
 }
