63c9e395053bca0d13c10a237f7f36e400215455
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:41:06 +0530

    Meal Director Class.

--- Builder/MealDirector.java
+++ Builder/MealDirector.java
@@ -1,2 +1,16 @@
-package Builder;public class MealDirector {
+package Builder;
+
+public class MealDirector {
+    private final MealBuilder BUILDER;
+
+    public MealDirector(MealBuilder builder){
+        this.BUILDER = builder;
+    }
+
+    public Meal construct_Meal(){
+        BUILDER.buildBurger();
+        BUILDER.buildDrink();
+        BUILDER.buildSide();
+        return BUILDER.getMeal();
+    }
 }
