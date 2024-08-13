fe60243d34e9515206629a073f041a3d0b49b4a8
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:14:11 +0530

    Observable Interface Example.

--- Observable/WeatherSystem.java
+++ Observable/WeatherSystem.java
@@ -1,2 +1,18 @@
-package Observable;public class WeatherSystem {
+package Observable;
+
+public class WeatherSystem {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        TemperatureDisplay temp = new TemperatureDisplay();
+        HumidityDisplay humidity = new HumidityDisplay();
+
+        weatherData.registerObserver(temp);
+        weatherData.registerObserver(humidity);
+
+        weatherData.setMeasurements(10.1f, 2.0f, 122.1f);
+
+        String weather = weatherData.toString();
+        System.out.println(weather);
+    }
 }
