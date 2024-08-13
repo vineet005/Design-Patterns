dd929d3ea4e7ea9d60c95894ddc8149d35bdc390
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:11:24 +0530

    Contains Method to Display Humidity.

--- Observable/HumidityDisplay.java
+++ Observable/HumidityDisplay.java
@@ -1,2 +1,14 @@
-package Observable;public class HumidityDisplay {
+package Observable;
+
+public class HumidityDisplay implements Observer{
+    private float humidity;
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.humidity = humidity;
+    }
+
+    public void display(){
+        System.out.println("Current Humidity is: "+humidity);
+    }
 }
+
