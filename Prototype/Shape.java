cc045fd223813cc587c84f6ec433e72ca51e5a26
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:49:35 +0530

    abstract class.

--- Prototype/Shape.java
+++ Prototype/Shape.java
@@ -1,2 +1,31 @@
-package Prototype;public class Shape {
+package Prototype;
+
+public abstract class Shape implements Cloneable{
+    private String id;
+    protected String type;
+
+    abstract void draw();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+        return clone;
+    }
 }
