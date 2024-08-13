33bb95c4f930c49df1f0f4687dcafc400ee4134f
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:50:34 +0530

    shapecache class.

--- Prototype/ShapeCache.java
+++ Prototype/ShapeCache.java
@@ -1,2 +1,22 @@
-package Prototype;public class ShapeCache {
+package Prototype;
+
+import java.util.HashMap;
+
+public class ShapeCache {
+    private static HashMap<String, Shape> shapeMap = new HashMap<>();
+
+    public static Shape getShape(String shapeID){
+        Shape cachedShape = shapeMap.get(shapeID);
+        return (Shape) cachedShape.clone();
+    }
+
+    public static void loadCache(){
+        Polygon polygon = new Polygon();
+        polygon.setId("1");
+        shapeMap.put(polygon.getId(), polygon);
+
+        Curve curve = new Curve();
+        curve.setId("2");
+        shapeMap.put(curve.getId(), curve);
+    }
 }
