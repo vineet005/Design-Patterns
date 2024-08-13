16a5b320fc8956d4c32fdbce2af3fecb3d6dbaf6
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:12:38 +0530

    Contains method to display temperature.

--- Observable/TemperatureDisplay.java
+++ Observable/TemperatureDisplay.java
@@ -1,2 +1,13 @@
-package Observable;public class TemperatureDisplay {
+package Observable;
+
+public class TemperatureDisplay implements Observer{
+    private float temperature;
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+    }
+
+    public void display(){
+        System.out.println("Current Temperature is: "+temperature+"C");
+    }
 }
