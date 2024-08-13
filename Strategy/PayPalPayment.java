87c8db2caa9a7fa91f1188f54898f65f31613211
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:53:07 +0530

    concrete class.

--- Strategy/PayPalPayment.java
+++ Strategy/PayPalPayment.java
@@ -1,2 +1,13 @@
-package Strategy;public class PayPalPayment {
+package Strategy;
+
+public class PayPalPayment implements PaymentStrategy{
+    private final String EMAIL;
+
+    public PayPalPayment(String email){
+        this.EMAIL = email;
+    }
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid Rs."+amount+" via PayPal!\n");
+    }
 }
