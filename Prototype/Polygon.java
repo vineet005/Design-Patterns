819146749441ae469d73b2f8a6159195433877e5
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:49:11 +0530

    polygon concrete class.

--- Prototype/Polygon.java
+++ Prototype/Polygon.java
@@ -1,2 +1,11 @@
-package Prototype;public class Polygon {
+package Prototype;
+
+public class Polygon extends Shape{
+    public Polygon(){
+        type = "Polygon";
+    }
+    @Override
+    void draw() {
+        System.out.println("Drawing Using Polygon..");
+    }
 }
