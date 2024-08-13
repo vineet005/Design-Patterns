a26e71bb96e581ffa9cd3186fbe14d32b91c6a94
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:48:48 +0530

    has main method.

--- Prototype/MainPrototype.java
+++ Prototype/MainPrototype.java
@@ -1,2 +1,13 @@
-package Prototype;public class MainPrototype {
+package Prototype;
+
+public class MainPrototype {
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape cloned1 =  ShapeCache.getShape("1");
+        System.out.println("Shape : " + cloned1.getType());
+
+        Shape cloned2 =  ShapeCache.getShape("2");
+        System.out.println("Shape : " + cloned2.getType());
+    }
 }
