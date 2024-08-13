51ec003a7568f576a37a5b35ce9c152b96b567af
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:23:00 +0530

    Abstract factory Design pattern.

--- AbstractFactory/MainTheme.java
+++ AbstractFactory/MainTheme.java
@@ -1,2 +1,26 @@
-package AbstractFactory;public class MainTheme {
+package AbstractFactory;
+
+public class MainTheme {
+    private Button button;
+    private TextField textField;
+
+    public MainTheme(GUIFactory factory){
+        button = factory.createButton();
+        textField = factory.createTextField();
+    }
+
+    public void paint(){
+        button.paint();
+        textField.paint();
+    }
+    public static void main(String[] args) {
+        GUIFactory dark = new DarkThemeFactory();
+        GUIFactory light = new LightThemeFactory();
+
+        MainTheme lmt = new MainTheme(light);
+        MainTheme dmt = new MainTheme(dark);
+
+        lmt.paint();
+        dmt.paint();
+    }
 }
