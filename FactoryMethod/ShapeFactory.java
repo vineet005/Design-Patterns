56eb46576c20c23b4ede66f7c4d0db7a653c7e80
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:17:02 +0530

    Factory class.

--- FactoryMethod/ShapeFactory.java
+++ FactoryMethod/ShapeFactory.java
@@ -1,2 +1,14 @@
-package FactoryMethod;public class ShapeFactory {
+package FactoryMethod;
+
+public class ShapeFactory {
+    public Shape getInstance(String shapeType){
+        if (shapeType.equalsIgnoreCase("Circle")){
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("Square")) {
+            return new Square();
+        }
+        else {
+            return null;
+        }
+    }
 }
