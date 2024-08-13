c50c028ca330b38088c2e4df6a97f01db2ee9875
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:44:38 +0530

    Concrete Plain Text CLass.

--- Decorator/PlainText.java
+++ Decorator/PlainText.java
@@ -1,8 +1,14 @@
 package Decorator;
 
-public class BoldText implements Text {
+public class PlainText implements Text {
+    private final String NEW_TEXT;
+
+    public PlainText(String text){
+        this.NEW_TEXT = text;
+    }
+
     @Override
     public String editText() {
-        return "**" + new Text() + "**";
+        return NEW_TEXT;
     }
 }
