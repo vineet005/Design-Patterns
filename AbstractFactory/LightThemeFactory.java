c3ae760a6f43aa016228ce392e8d31bf99b16b41
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:22:04 +0530

    Factory class for light theme.

--- AbstractFactory/LightThemeFactory.java
+++ AbstractFactory/LightThemeFactory.java
@@ -1,2 +1,16 @@
-package AbstractFactory;public class LightThemeFactory {
+package AbstractFactory;
+
+public class LightThemeFactory extends GUIFactory{
+    @Override
+    Button createButton() {
+        System.out.println("Light Button Created!");
+        return new LightButton();
+
+    }
+
+    @Override
+    TextField createTextField() {
+        System.out.println("Light TextField Created!");
+        return new LightTextField();
+    }
 }
