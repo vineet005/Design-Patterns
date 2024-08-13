b21e6dfe74344a92114fc3b4ca71947f36e85b11
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:44:07 +0530

    Contains Main Method.

--- Decorator/MainText.java
+++ Decorator/MainText.java
@@ -1,2 +1,48 @@
-package Decorator;public class MainText {
+package Decorator;
+
+import java.util.Scanner;
+
+public class MainText {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Text text = new PlainText("Hello World!");
+
+        while (true){
+            System.out.println("1. Make the text  Bold.");
+            System.out.println("2. Make the text  Italic.");
+            System.out.println("3. Make the text  Underlined.");
+            System.out.println("4. Exit.");
+            System.out.println("Choose What You Want to do With The Text: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    text = new BoldText(text);
+                    String boldString = text.editText();
+                    System.out.println(boldString);
+                    break;
+
+                case 2:
+                    text = new ItalicText(text);
+                    String italicString = text.editText();
+                    System.out.println(italicString);
+                    break;
+
+                case 3:
+                    text = new UnderlineText(text);
+                    String underlinedString = text.editText();
+                    System.out.println(underlinedString);
+                    break;
+
+                case 4:
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid Input!");
+            }
+
+        }
+    }
 }
