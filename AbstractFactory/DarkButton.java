70fbb329d8fc0dfff21ca5b50a110fe66a117bfa
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:19:27 +0530

    dark Button Class.

--- AbstractFactory/DarkButton.java
+++ AbstractFactory/DarkButton.java
@@ -1,2 +1,8 @@
-package AbstractFactory;public class DarkButton {
+package AbstractFactory;
+
+public class DarkButton implements Button{
+    @Override
+    public void paint() {
+        System.out.println("Button in Dark Mode!");
+    }
 }
