9e9085dce2f5be20255585bdc008197287199b41
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:52:28 +0530

    context class.

--- Strategy/PaymentContext.java
+++ Strategy/PaymentContext.java
@@ -1,2 +1,13 @@
-package Strategy;public class PaymentContext {
+package Strategy;
+
+public class PaymentContext {
+    private PaymentStrategy payment_Strategy;
+
+    public void setPayment_Strategy(PaymentStrategy payment_Strategy){
+        this.payment_Strategy = payment_Strategy;
+    }
+
+    public void processPayment(double amount){
+        payment_Strategy.pay(amount);
+    }
 }
