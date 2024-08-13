8fc6de4f62609bf2675c9a305d55f7c0250d9c1f
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:51:16 +0530

    Concrete bankaccount class

--- Strategy/BankAccountPayment.java
+++ Strategy/BankAccountPayment.java
@@ -1,2 +1,13 @@
-package Strategy;public class BankAccountPayment {
+package Strategy;
+
+public class BankAccountPayment implements PaymentStrategy{
+    private final String ACCOUNT_NUMBER;
+
+    public BankAccountPayment(String account_Number){
+        this.ACCOUNT_NUMBER = account_Number;
+    }
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid Rs."+amount+" through Bank Account!\n");
+    }
 }
