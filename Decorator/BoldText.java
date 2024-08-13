b0d2509f711bf5ef0b0f528d1911987299acb2e4
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:43:18 +0530

    Bold Concrete Class.

--- Decorator/BoldText.java
+++ Decorator/BoldText.java
@@ -1,2 +1,12 @@
-package Decorator;public class BoldText {
+package Decorator;
+
+public class BoldText extends AbstractText{
+    public BoldText(Text text){
+        super(text);
+    }
+
+    @Override
+    public String editText() {
+        return "**"+NEW_TEXT.editText()+"**";
+    }
 }
