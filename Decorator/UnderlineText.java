723709d5063c84f72ef81a8c4049b46b39ad14a3
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:46:08 +0530

    Concrete UnderlineText Class.

--- Decorator/UnderlineText.java
+++ Decorator/UnderlineText.java
@@ -1,2 +1,12 @@
-package Decorator;public class UnderlineText {
+package Decorator;
+
+public class UnderlineText extends AbstractText{
+    public UnderlineText(Text newText) {
+        super(newText);
+    }
+
+    @Override
+    public String editText() {
+        return "__"+NEW_TEXT.editText()+"__";
+    }
 }
