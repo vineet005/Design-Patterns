eba5156efc449ceff0b9329ca8dfaf2b38727efe
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:17:19 +0530

    Square class.

--- FactoryMethod/Square.java
+++ FactoryMethod/Square.java
@@ -1,2 +1,8 @@
-package FactoryMethod;public class Square {
+package FactoryMethod;
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Square!");
+    }
 }
