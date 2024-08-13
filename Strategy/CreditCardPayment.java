99b4e150af832c4aded7d7acf698cd27dbc7107a
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 23:51:47 +0530

    concrete class.

--- Strategy/CreditCardPayment.java
+++ Strategy/CreditCardPayment.java
@@ -1,2 +1,16 @@
-package Strategy;public class CreditCardPayment {
+package Strategy;
+
+public class CreditCardPayment implements PaymentStrategy{
+    private final String CREDIT_CARD_NUMBER;
+    private final String CARD_HOLDER_NAME;
+
+    public CreditCardPayment(String credit_Card_Number, String card_Holder_Name){
+        this.CREDIT_CARD_NUMBER = credit_Card_Number;
+        this.CARD_HOLDER_NAME = card_Holder_Name;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid Rs."+amount+" using Credit Card!\n");
+    }
 }
