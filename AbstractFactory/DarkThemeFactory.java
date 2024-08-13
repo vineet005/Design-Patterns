24e1dcc6095acfc88a5a8add6288ade0312f51b0
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-13 17:20:33 +0530

    factory class for dark theme.

--- AbstractFactory/DarkThemeFactory.java
+++ AbstractFactory/DarkThemeFactory.java
@@ -1,2 +1,15 @@
-package AbstractFactory;public class DarkThemeFactory {
+package AbstractFactory;
+
+public class DarkThemeFactory extends GUIFactory{
+    @Override
+    Button createButton() {
+        System.out.println("Dark Button Created!");
+        return new DarkButton();
+    }
+
+    @Override
+    TextField createTextField() {
+        System.out.println("Dark TextField Created!");
+        return new DarkTextField();
+    }
 }
